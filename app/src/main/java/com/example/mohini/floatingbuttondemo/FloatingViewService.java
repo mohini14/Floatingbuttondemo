7bb6f8f28ecf58a5571c9c9171f75327f2c8b634
Mohini Sindhu <mohini.sindhu14@gmail.com>
2018-01-05 13:57:08 +0530

    permissions functionalities

--- app/src/main/java/com/example/mohini/floatingbuttondemo/FloatingViewService.java
+++ app/src/main/java/com/example/mohini/floatingbuttondemo/FloatingViewService.java
@@ -3,22 +3,33 @@ package com.example.mohini.floatingbuttondemo;
 import android.app.Service;
 import android.content.Intent;
 import android.graphics.PixelFormat;
+import android.media.Image;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 /**
  * Created by mohini on 04/01/18.
  */
 
-public class FloatingViewService extends Service {
+public class FloatingViewService extends Service implements View.OnTouchListener {
 
     private WindowManager mWindowManager;
     private View mFloatingView;
 
+   private View collapsedView = null;
+    private View expandedView = null;
+
+    float initialX;
+    float initialY;
+    float initialTouchX;
+    float initialTouchY;
 
     public FloatingViewService(){
 
@@ -36,6 +47,8 @@ public class FloatingViewService extends Service {
         super.onCreate();
         mFloatingView = LayoutInflater.from(this).inflate(R.layout.layout_floating_widget, null);
 
+        collapsedView = mFloatingView.findViewById(R.id.collapse_view);
+        expandedView = mFloatingView.findViewById(R.id.expanded_container);
         // adding view to window
         final WindowManager.LayoutParams params = new WindowManager.LayoutParams(
                 WindowManager.LayoutParams.WRAP_CONTENT,
@@ -53,8 +66,106 @@ public class FloatingViewService extends Service {
         // add the view to window
         mWindowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
         mWindowManager.addView(mFloatingView, params);
+
+
+        setCloseButton();
+        setPlayButton();
+        setNextButon();
+        setPreviousButton();
+        setCloseBtn();
+        setOpenButton();
+
+        handleTouch(params);
     }
 
+    // set buttons
+    private void setCloseButton(){
+
+
+        // setting the closed button
+        ImageView closedButtonCollapsed = (ImageView) mFloatingView.findViewById(R.id.close_button);
+
+        // set on click listner
+        closedButtonCollapsed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //close the service and remove the from from the window
+                stopSelf();
+            }
+        });
+    }
+
+    private void setPlayButton(){
+
+        // set the view while view is expanded
+
+        ImageView playButton = (ImageView) mFloatingView.findViewById(R.id.play_btn);
+        playButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+
+                Toast.makeText(FloatingViewService.this, "Playing button expanded", Toast.LENGTH_LONG);
+            }
+        });
+    }
+
+    private void setNextButon(){
+
+        ImageView nextButton = (ImageView) mFloatingView.findViewById(R.id.next_btn);
+        nextButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+                Toast.makeText(FloatingViewService.this, "Playing next song.", Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+    private void setPreviousButton(){
+
+        //Set the pause button.
+        ImageView prevButton = (ImageView) mFloatingView.findViewById(R.id.prev_button);
+        prevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FloatingViewService.this, "Playing previous song.", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void setCloseBtn(){
+        //Set the close button
+        ImageView closeButton = (ImageView) mFloatingView.findViewById(R.id.close_btn);
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                collapsedView.setVisibility(View.VISIBLE);
+                expandedView.setVisibility(View.GONE);
+            }
+        });
+
+    }
+
+    private void setOpenButton(){
+
+        ImageView openButton = (ImageView) mFloatingView.findViewById(R.id.open_button);
+        openButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Open the application  click.
+                Intent intent = new Intent(FloatingViewService.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+
+
+                //close the service and remove view from the view hierarchy
+                stopSelf();
+            }
+        });
+    }
     @Override
     public void onDestroy(){
 
@@ -62,4 +173,60 @@ public class FloatingViewService extends Service {
         if(mFloatingView != null) mWindowManager.removeView(mFloatingView);
     }
 
+    private void handleTouch(final WindowManager.LayoutParams params){
+    mFloatingView.findViewById(R.id.root_container).setOnTouchListener(new View.OnTouchListener(){
+
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+
+
+                    //remember the initial position.
+                    initialX = params.x;
+                    initialY = params.y;
+
+
+                    //get the touch location
+                    initialTouchX = event.getRawX();
+                    initialTouchY = event.getRawY();
+                    return true;
+                case MotionEvent.ACTION_MOVE:
+                    //Calculate the X and Y coordinates of the view.
+                    params.x = (int) (initialX + (int) (event.getRawX() - initialTouchX));
+                    params.y = (int) (initialY + (int) (event.getRawY() - initialTouchY));
+
+
+                    //Update the layout with new X & Y coordinate
+                    mWindowManager.updateViewLayout(mFloatingView, params);
+                    return true;
+
+                case MotionEvent.ACTION_UP:
+
+                    int Xdiff = (int) (event.getRawX() - initialTouchX);
+                    int Ydiff = (int) (event.getRawY() - initialTouchY);
+
+                    if(Xdiff < 10 && Ydiff < 10){
+
+                        if(isViewCollapsed()){
+
+                            collapsedView.setVisibility(View.GONE);
+                            expandedView.setVisibility(View.VISIBLE);
+                        }
+                    }
+            }
+            return false;
+        }
+    });
+}
+
+        private boolean isViewCollapsed(){
+            return mFloatingView == null || mFloatingView.findViewById(R.id.collapse_view).getVisibility() == View.VISIBLE;
+        }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        return false;
+    }
 }
--- app/src/main/java/com/example/mohini/floatingbuttondemo/MainActivity.java
+++ app/src/main/java/com/example/mohini/floatingbuttondemo/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.mohini.floatingbuttondemo;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,59 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        HandlePermissions();
+//        initialiseView();
+    }
+
+    private void HandlePermissions() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
+
+            try {
+                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                        Uri.parse("package" + getPackageName( )));
+
+                startActivityForResult(intent, 12);
+            }
+            catch (Exception e){
+
+            }
+        } else {
+
+            initialiseView( );
+        }
+    }
+
+    private void initialiseView() {
+
+        findViewById(R.id.notif_me).setOnClickListener(new View.OnClickListener( ) {
+
+            @Override
+            public void onClick(View v) {
+
+                startService(new Intent(MainActivity.this, FloatingViewService.class));
+                finish( );
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == 12) {
+
+            if (resultCode == RESULT_OK)
+                initialiseView( );
+
+            else { //Permission is not available
+                Toast.makeText(this,
+                        "Draw over other app permission not available. Closing the application",
+                        Toast.LENGTH_SHORT).show( );
+                finish( );
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
